f33a0a8531c8b31595c2fcf4506ec569546ed5f0
romannamgaladze <roman.namgaladze@gmail.com>
2025-05-18 11:14:13 +0400

    animations
    animations added

---
+++ index.js
@@ -0,0 +1,89 @@
+// {
+//   const a = 20;
+//   var b = 300;
+//   let c = 200;
+// }
+// let c = 400;
+// console.log(b);
+// console.log(c);
+// console.log(a);
+
+// let variable = 2;
+// let chemicvladi;
+
+// typeof variable === "";
+// console.log(typeof chemicvladi === "number");
+
+// function greeting(name) {
+//   console.log("Hello", name);
+// }
+// greeting("Luka");
+// greeting("Revaz");
+
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+// sum(300, 5000);
+// sum(100, 2490);
+
+// function sentences(word1 = "", word2 = "", word3 = "", word4 = "") {
+//   console.log(word1, word2, word3, word4);
+// }
+// sentences("ioane", "aris", "web");
+
+// function sumNumbers(a = 0, b = 0, c = 0, d = 0) {
+//   console.log(a + b + c + d);
+// }
+// sumNumbers(1, 2, 3);
+
+// function singlefunction() {
+//   console.log("Hellow World");
+// }
+// singlefunction();
+
+// let a = 2;
+// let b = 8;
+
+// function myfunction() {
+//   console.log(a + b);
+// }
+// myfunction();
+
+// function multiplenumber(x, y) {
+//   console.log(x * y);
+//   return x * y;
+// }
+// multiplenumber(2, 2);
+
+// console.log(multiplenumber(5, 2));
+
+// let result1 = multiplenumber(3, 7);
+// console.log(result1);
+
+// let a = 20;
+// let b = 30;
+// function myfunction() {
+//   console.log(a + b);
+//   b += 30;
+// }
+// // myfunction();
+// // myfunction();
+// // myfunction();
+// // myfunction();
+// // myfunction();
+// // myfunction();
+// // myfunction();
+// // myfunction();
+// // myfunction();
+
+// function function2() {
+//   console.log(a * b);
+// }
+// function2();
+
+let chemifunqcia = function (a, b) {
+  console.log(a + b);
+  return 200;
+};
+chemifunqcia(20, 30);
+console.log(chemifunqcia(20, 30));
